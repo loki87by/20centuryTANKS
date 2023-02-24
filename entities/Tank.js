55da70cb5efb93af09d1edc639422cd6988fd8ea
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2023-02-24 14:01:53 +0300

    Update Tank.js

--- entities/Tank.js
+++ entities/Tank.js
@@ -9,7 +9,7 @@ export default class Tank extends Entity {
     this.id = id;
     this.command = command;
     this.health = 100;
-    this.speed = speed || Math.floor(10 * POINT);
+    this.speed = speed || Math.floor(1 * POINT);
     this.gamer = gamer || "auto";
     this.hasGun = false;
     this.isSheep = false;
@@ -56,7 +56,7 @@ export default class Tank extends Entity {
       dx: 0,
       dy: 0
     }
-    const speed = this.hasStar || this.hasGun ? Math.floor(30 * POINT) : Math.floor(25 * POINT)
+    const speed = this.hasStar || this.hasGun ? Math.floor(6 * POINT) : Math.floor(5 * POINT)
 
     if (this.direction === "south") {
       coords.x += Math.floor(this.getData().width / 2);
