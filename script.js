775f80594ab7145bc26a9203d881230c7e15af12
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2023-02-24 14:00:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -290,26 +290,13 @@ function loop() {
     bullet.replace(data.coords.x + data.dx, data.coords.y + data.dy);
     MAP.forEach((item) => {
       if (collides(item, bullet.getData().prestep)) {
-        shiftToTarget(bullet, MAP, data.direction);
-        console.log(item, bullet)
-        /* const top = MAP.find((i) => {
-          if(i.coords.x === item.coords.x && i.coords.y === item.coords.y - item.height) {
-            return i}})
-        const bottom = MAP.find((i) => {
-          if(i.coords.x === item.coords.x && i.coords.y === item.coords.y + item.height) {
-            return i}})
-        const left = MAP.find((i) => {
-          if(i.coords.x === item.coords.x - item.width && i.coords.y === item.coords.y) {
-            return i}})
-        const right = MAP.find((i) => {
-          if(i.coords.x === item.coords.x + item.width && i.coords.y === item.coords.y) {
-            return i}})*/
         const top = MAP.find(i => i.coords.x === item.coords.x && i.coords.y === item.coords.y - item.height)
         const bottom = MAP.find(i => i.coords.x === item.coords.x && i.coords.y === item.coords.y + item.height)
         const left = MAP.find(i => i.coords.x === item.coords.x - item.width && i.coords.y === item.coords.y)
         const right = MAP.find(i => i.coords.x === item.coords.x + item.width && i.coords.y === item.coords.y)
         const friendlyItems = [top, bottom, left, right].filter(i => i !== undefined)
         console.log(friendlyItems)
+        shiftToTarget(bullet, MAP, data.direction);
         item.destroy(data.direction)
         friendlyItems.forEach((friend) => {
           if (collides(friend, bullet.getData())) {
